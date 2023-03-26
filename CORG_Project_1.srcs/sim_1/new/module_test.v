69af000bd3c125cc27cbedda07aab78647a75c6a
mymermer <myusufmermer@gmail.com>
2023-03-26 13:39:33 +0300

    for loops for test of 1st register

--- CORG_Project_1.srcs/sim_1/new/module_test.v
+++ CORG_Project_1.srcs/sim_1/new/module_test.v
@@ -8,54 +8,75 @@ module register_tb;
     reg [1:0] funsel;
     reg [N-1:0] load;
     wire [N-1:0] Q_out;
-
+   
     register #(.N(N)) my_reg (.clk(clk), .enable(enable), .funsel(funsel), .load(load), .Q_out(Q_out));
 
+    integer i,j,k;
+    initial begin
+        load=8'd1;
+        clk=0;
+        for (i = 1; i > -1; i = i - 1) begin
+            for (j = 0; j < 2; j = j + 1) begin
+                for (k=0;k<2;k=k+1) begin
+                    // for (l = 0; l < 2**N; l = l + 1) begin
+                    //     for (n = 0; n < N; n = n + 1) begin
+                    //         load[n] = (l >> n) & 1'b1;
+                    //     end
+                        enable = i;
+                        funsel[1] = j;
+                        funsel[0] = k;
+
+                        #125;
+                    // end
+                end
+            end
+        end
+    end
     always #5 clk = ~clk;
 
-    initial begin
-        enable = 1;
-        funsel = 2'b00;
-        load = 8'b00;
-        clk = 0;
 
-        #10;
-        load = 8'b01;
-        funsel = 2'b01;
-        
-        #10;
-        load = 8'b11;
-        funsel = 2'b11;
-        
-        #10;
-        load = 8'b10;
-        funsel = 2'b00;
-        
-        #10;
-        load = 8'b11;
-        funsel = 2'b01;
-        
-        #10;
-        load = 8'b11;
-        funsel = 2'b10;
-        
-        #10;
-        load = 8'b10;
-        funsel = 2'b10;
+        // enable = 1;
+        // funsel = 2'b00;
+        // load = 8'b00;
+        // clk = 0;
 
-        #10;
-        load = 8'b10;
-        funsel = 2'b10;
+        // #10;
+        // load = 8'b01;
+        // funsel = 2'b01;
         
-        #10;
-        load = 8'b10;
-        funsel = 2'b10;
+        // #10;
+        // load = 8'b11;
+        // funsel = 2'b11;
         
-        #10;
-        enable = 0;
+        // #10;
+        // load = 8'b10;
+        // funsel = 2'b00;
         
-        #10;
-        enable = 1;
-        #10 $finish;
-    end
+        // #10;
+        // load = 8'b11;
+        // funsel = 2'b01;
+        
+        // #10;
+        // load = 8'b11;
+        // funsel = 2'b10;
+        
+        // #10;
+        // load = 8'b10;
+        // funsel = 2'b10;
+
+        // #10;
+        // load = 8'b10;
+        // funsel = 2'b10;
+        
+        // #10;
+        // load = 8'b10;
+        // funsel = 2'b10;
+        
+        // #10;
+        // enable = 0;
+        
+        // #10;
+        // enable = 1;
+        // #10 $finish;
+//    end
 endmodule
