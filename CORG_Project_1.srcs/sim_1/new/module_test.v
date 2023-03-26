7208601f650f8991e4cb36ab3e5fd57f083fa8d7
mymermer <myusufmermer@gmail.com>
2023-03-26 10:06:34 +0300

    first commit

---
+++ CORG_Project_1.srcs/sim_1/new/module_test.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 26.03.2023 09:11:49
+// Design Name: 
+// Module Name: module_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module module_test(
+
+    );
+endmodule
