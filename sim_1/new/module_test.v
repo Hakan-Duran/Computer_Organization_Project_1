aed1c5acb77bad5ed6141cc4c737d670d7bee98e
Hakan-Duran <hakanduran@zohomail.eu>
2023-03-31 17:21:21 +0300

    Register (part1) has been changed.

--- sim_1/new/module_test.v
+++ sim_1/new/module_test.v
@@ -3,37 +3,34 @@
 module register_tb;
     
     parameter N = 8;
-    reg clk;
     reg enable;
     reg [1:0] funsel;
     reg [N-1:0] load;
     wire [N-1:0] Q_out;
    
-    register #(.N(N)) my_reg (.clk(clk), .enable(enable), .funsel(funsel), .load(load), .Q_out(Q_out));
-
-    integer i,j,k;
+    register #(.N(N)) my_reg (.enable(enable), .funsel(funsel), .load(load), .Q_out(Q_out));
+    
     initial begin
-        load=8'd1;
-        clk=0;
-        for (i = 1; i > -1; i = i - 1) begin
-            for (j = 0; j < 2; j = j + 1) begin
-                for (k=0;k<2;k=k+1) begin
-                    // for (l = 0; l < 2**N; l = l + 1) begin
-                    //     for (n = 0; n < N; n = n + 1) begin
-                    //         load[n] = (l >> n) & 1'b1;
-                    //     end
-                        enable = i;
-                        funsel[1] = j;
-                        funsel[0] = k;
-
-                        #125;
-                    // end
-                end
-            end
-        end
+        load=8'b10010101;
+        
+        enable = 1;
+        funsel = 2'b00;
+        
+        #10;
+        
+        funsel = 2'b01;
+        
+        #10;
+        
+        funsel = 2'b10;
+        
+        #10;
+        
+        funsel = 2'b00;
+        
+        #10;
+        
+        $finish;
     end
-    always #5 clk = ~clk;
-
-
 
 endmodule
--- sources_1/new/modules.v
+++ sources_1/new/modules.v
@@ -1,13 +1,12 @@
 `timescale 1ns / 1ps
 
-module register #(parameter N=2)(clk, enable, funsel, load, Q_out);
-input clk;
+module register #(parameter N=2)(enable, funsel, load, Q_out);
 input enable;
 input [1:0] funsel;
 input [N-1:0] load;
 output reg [N-1:0] Q_out;
 
-always @(posedge clk) begin
+always @(enable, funsel) begin
     if (enable) begin
        case (funsel)
         2'b00 : Q_out <= {N{1'b0}} ;
@@ -15,7 +14,7 @@ always @(posedge clk) begin
         2'b10 : Q_out <= Q_out - {{(N-1){1'b0}}, 1'b1} ;
         2'b11 : Q_out <= Q_out + {{(N-1){1'b0}}, 1'b1} ;
         default : Q_out <= load ;
-        endcase 
-    end
+        endcase
+     end
 end
 endmodule
