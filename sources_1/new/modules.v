0c85d73197d8358c74e1435f61747f463bf22f11
Hakan-Duran <hakanduran@zohomail.eu>
2023-03-31 17:40:07 +0300

    ir (part2.1) added

--- sources_1/new/modules.v
+++ sources_1/new/modules.v
@@ -8,13 +8,39 @@ output reg [N-1:0] Q_out;
 
 always @(enable, funsel) begin
     if (enable) begin
-       case (funsel)
-        2'b00 : Q_out <= {N{1'b0}} ;
-        2'b01 : Q_out <= load ;
-        2'b10 : Q_out <= Q_out - {{(N-1){1'b0}}, 1'b1} ;
-        2'b11 : Q_out <= Q_out + {{(N-1){1'b0}}, 1'b1} ;
-        default : Q_out <= load ;
-        endcase
+      case (funsel)
+         2'b00 : Q_out <= {N{1'b0}} ;
+         2'b01 : Q_out <= load ;
+         2'b10 : Q_out <= Q_out - {{(N-1){1'b0}}, 1'b1} ;
+         2'b11 : Q_out <= Q_out + {{(N-1){1'b0}}, 1'b1} ;
+         default : Q_out <= load ;
+      endcase
      end
 end
+endmodule
+
+module ir (
+   data, enable, funsel, lh, irout
+);
+
+input [7:0] data;
+input [1:0] funsel;
+input enable;
+input lh;
+output reg [15:0] irout;
+
+always @(enable, funsel) begin
+   if (enable) begin
+      case (funsel)
+         2'b00 : irout <= 16b'0; 
+         2'b01 : begin 
+            if (!lh) irout[7:0] <= data ;
+            else irout[15:8] <= data ;
+         end
+         2'b10 : irout <= irout - {15'b0, 1'b1} ;
+         2'b11 : irout <= irout + {15'b0, 1'b1} ;
+      endcase
+   end
+end
+   
 endmodule
