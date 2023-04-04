b5781359f8b4eee6d2a812eb539f6601ec71b143
mymermer <myusufmermer@gmail.com>
2023-04-04 18:26:02 +0300

    system's simulation file is written but error correction needed

--- sim_1/new/module_test.v
+++ sim_1/new/module_test.v
@@ -224,3 +224,82 @@ alu my_alu (.A(A), .B(B), .Funsel(Funsel), .Flag(Flag), .OutALU(OutALU));
 
 
 endmodule
+
+
+
+module system_test();
+   reg [1:0] outasel;
+   reg[1:0] outbsel;
+   reg [1:0] funsel_IR;
+   reg [1:0] funsel_arf;
+   reg [1:0] funsel_rf;
+   reg [3:0] funsel_alu;
+   reg [3:0] regsel_rf;
+   reg [3:0] regsel_arf;
+   reg clock;
+   reg wrMEM;
+   reg csMEM;
+   reg IR_enable;
+   reg IR_lh;
+   reg [1:0] MUXSelA;
+   reg [1:0] MUXSelB;
+   reg MUXSelC;
+   reg [2:0]rf_o1sel;
+   reg [2:0]rf_o2sel;
+   reg [3:0]rf_tsel;
+
+
+
+   wire[7:0] IR_out_MSBs;
+
+
+    system sys1( outasel, outbsel, funsel_IR, funsel_arf, funsel_rf, funsel_alu,regsel_rf,regsel_arf,  clock,  wrMEM,  csMEM,  IR_enable,  IR_lh,  MUXSelA,  MUXSelB,  MUXSelC,  rf_o1sel,   rf_o2sel,   rf_tsel, IR_out_MSBs );
+
+
+
+initial begin
+    clock=0;
+    outasel=2'b00;
+    outbsel=2'b01;
+    funsel_IR=2'b01;
+    funsel_arf=2'b11;
+    funsel_rf=2'b01;
+    funsel_alu=4'b1010;
+    regsel_rf=4'b0100;
+    regsel_arf=4'b0010;
+    wrMEM=1;
+    csMEM=0;
+    IR_enable=1;
+    IR_lh=1;
+    MUXSelA=2'b00;
+    MUXSelB=2'b10;
+    MUXSelC=1;
+    rf_o1sel =3'b010;
+    rf_o2sel =3'b110;
+    rf_tsel=4'b1110;
+
+end    
+
+always #3.5 clock=~clock;
+always #9.3 outasel=outasel+2'b01;
+always #8.6 outbsel=outasel+2'b01;
+always #7 funsel_IR = funsel_IR+2'b01;
+always #6 funsel_arf=funsel_arf+2'b01;
+always #6.2 funsel_rf=funsel_rf+2'b01;
+always #4 funsel_alu = funsel_alu+4'b0001;
+always #5 regsel_rf =regsel_rf + 4'b0001;
+always #5.3 regsel_arf=regsel_arf+4'b0001;
+always #4.4 wrMEM=~wrMEM;
+// always #7.1 csMEM=~csMEM;
+// always #3.9 IR_enable=~IR_enable;
+always #5.8 IR_lh=~IR_lh;
+always #5.2 MUXSelA=MUXSelA+2'b01;
+always #4.8 MUXSelB=MUXSelB+2'b01;
+always #5.2 MUXSelC=~MUXSelC;
+always #4.7 rf_o1sel=rf_o1sel+3'b001;
+always #5.7 rf_o2sel=rf_o2sel+3'b001;
+always #6.4  rf_tsel=rf_tsel+4'b0001;
+
+
+
+endmodule
